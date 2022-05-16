b751356a784e1b89b7f3406a019cdf73735af892
ahmetsoydogan <102738160+ahmetsoydogan@users.noreply.github.com>
2022-05-16 17:20:04 +0300

    Add files via upload

---
+++ Poisson.m
@@ -0,0 +1,29 @@
+clc, clear, close all
+
+
+%POISSON CODE
+
+possPMF(2,1.8)
+possCDF(2,1.8)
+
+function possPMF = possPMF(k,l)
+    e = exp(1);
+    
+    possPMF = ((l^k)*(e^(-l)))/factorial(k);
+
+end
+
+
+function possCDF = possCDF(x,l)
+    e = exp(1);
+    V = uint32(1):uint32(x);
+    values_array=[];
+
+    for k= 1:1:x
+        values_array(k)= (((l^k)*(e^(-l)))/factorial(k))*100;
+    end
+        bar(V,values_array);
+        title('Percentage of occurance in intended trial');
+        xlabel('Percentage (%)');
+        ylabel('Intended Trials');
+end
